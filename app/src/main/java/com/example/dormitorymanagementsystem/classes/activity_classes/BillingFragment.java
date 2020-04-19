75cdd21d7117fa099b327960015370aed4edc85b
pjaescarilla <pjaescarilla@gmail.com>
2020-04-20 05:47:07 +0800

    all functions working + paymaya integration initialized

--- app/src/main/java/com/example/dormitorymanagementsystem/classes/activity_classes/BillingFragment.java
+++ app/src/main/java/com/example/dormitorymanagementsystem/classes/activity_classes/BillingFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,11 +21,23 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.paymaya.sdk.android.PayMayaConfig;
+import com.paymaya.sdk.android.checkout.PayMayaCheckout;
+import com.paymaya.sdk.android.checkout.PayMayaCheckoutCallback;
+import com.paymaya.sdk.android.checkout.models.Address;
+import com.paymaya.sdk.android.checkout.models.Buyer;
+import com.paymaya.sdk.android.checkout.models.Checkout;
+import com.paymaya.sdk.android.checkout.models.Contact;
+import com.paymaya.sdk.android.checkout.models.Item;
+import com.paymaya.sdk.android.checkout.models.RedirectUrl;
+import com.paymaya.sdk.android.checkout.models.TotalAmount;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BillingFragment extends Fragment {
+public class BillingFragment extends Fragment implements PayMayaCheckoutCallback {
 
     // View Variables
     private View thisView;
@@ -35,9 +48,15 @@ public class BillingFragment extends Fragment {
     // Database Variables
     private DatabaseReference billingInfoTable, transactionsTable;
 
+    // Paymaya Variables
+    private static final String PUBLIC_FACING_API_KEY = "pk-eo4sL393CWU5KmveJUaW8V730TTei2zY8zE4dHJDxkF";
+    private PayMayaCheckout mPayMayaCheckout;
+
     // Other Variables
     private Bill thisProfilesBill;
 
+
+    // OVERRIDE METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -46,15 +65,34 @@ public class BillingFragment extends Fragment {
         initializeVariables();
         populateFields();
 
-        buttonPay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recordTransaction();
-            }
-        });
+        //buttonPay.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        recordTransaction();
+        //    }
+        //});
+
+        testPaymaya();
 
         return thisView;
     }
+
+    @Override
+    public void onCheckoutSuccess() {
+        Toast.makeText(getContext(), "Payment Successful", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCheckoutCanceled() {
+        Toast.makeText(getContext(), "Payment Cancelled", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCheckoutFailure(String message) {
+        Toast.makeText(getContext(), "Payment Failed", Toast.LENGTH_LONG).show();
+    }
+
+    // METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     private void initializeVariables()
     {
@@ -132,5 +170,54 @@ public class BillingFragment extends Fragment {
             thisProfilesBill.setBalance(newBalance);
             billingInfoTable.child(((DormVars)getActivity().getApplication()).getActiveProfile().getUsername()).setValue(thisProfilesBill);
         }
+    }
+
+    private void testPaymaya() {
+        PayMayaConfig.setEnvironment(PayMayaConfig.ENVIRONMENT_SANDBOX);
+        mPayMayaCheckout = new PayMayaCheckout(PUBLIC_FACING_API_KEY, this);
+        Contact contact = new Contact("09051233249", "supbish22@gmail.com");
+        Address address = new Address("line 1", "line 2", "city", "state", "zip code", "PH");
+        Buyer buyer = new Buyer("First name", "Middle name", "Last name");
+        buyer.setContact(contact);
+        buyer.setBillingAddress(address);
+        buyer.setShippingAddress(address);
+
+        BigDecimal summaryTotal = BigDecimal.valueOf(0);
+        List itemsList = new ArrayList<>();
+        String currency = "PHP";
+
+        BigDecimal item1Amount = BigDecimal.valueOf(100);
+        summaryTotal.add(item1Amount);
+        TotalAmount totalAmount = new TotalAmount(item1Amount, currency);
+        int quantity = 10;
+        Item item1 = new Item("Item 1 name", quantity, totalAmount);
+        item1.setSkuCode("SKU code");
+        item1.setDescription("bag");
+        itemsList.add(item1);
+
+        BigDecimal item2Amount = BigDecimal.valueOf(200);
+        summaryTotal.add(item2Amount);
+        totalAmount = new TotalAmount(item2Amount, currency);
+        quantity = 20;
+        Item item2 = new Item("Item 2 name", quantity, totalAmount);
+        item2.setSkuCode("SKU code");
+        item2.setDescription("shoes");
+        itemsList.add(item2);
+
+        String successURL = "http://yourshop.com/success";
+        String failedURL = "http://yourshop.com/failed";
+        String canceledURL = "http://yourshop.com/canceled";
+
+        RedirectUrl redirectUrl = new RedirectUrl(successURL, failedURL, canceledURL);
+
+        String requestReference = "asdfsdr1234asdfwe";
+        final Checkout checkout = new Checkout(totalAmount,buyer,itemsList,requestReference,redirectUrl);
+
+        buttonPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPayMayaCheckout.execute(getActivity(),checkout);
+            }
+        });
     }
 }
